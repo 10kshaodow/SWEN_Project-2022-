3850a548437ce5dc41beccc9a465ef27e973e5ea
Colin Tondreau <colin.d.m.tondreau@gmail.com>
2022-09-23 21:50:13 -0400

    services and all-inventory compenents created

---
+++ estore-ui/src/app/components/all-inventory.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AllInventoryComponent } from './all-inventory.component';
+
+describe('AllInventoryComponent', () => {
+  let component: AllInventoryComponent;
+  let fixture: ComponentFixture<AllInventoryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AllInventoryComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AllInventoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ estore-ui/src/app/components/all-inventory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-inventory',
+  templateUrl: './all-inventory.component.html',
+  styleUrls: ['./all-inventory.component.css']
+})
+export class AllInventoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
