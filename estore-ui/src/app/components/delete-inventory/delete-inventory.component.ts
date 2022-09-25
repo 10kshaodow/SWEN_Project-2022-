bde230c46f84b90c633c50540dfb32bea306afd5
adl8493 <adl8493@g.rit.edu>
2022-09-25 14:06:00 -0400

    Delete API call implemented. HTML needs to be revised.

---
+++ estore-ui/src/app/components/delete-inventory/delete-inventory.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product/product.service';
+import { Product } from '../../types/Product';
+
+@Component({
+  selector: 'app-delete-inventory',
+  templateUrl: './delete-inventory.component.html',
+  styleUrls: ['./delete-inventory.component.css']
+})
+export class DeleteInventoryComponent implements OnInit {
+
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.deleteInventory();
+    
+   
+  }
+
+  deleteInventory() {
+    this.productService.deleteProduct()
+  }
+
+}
