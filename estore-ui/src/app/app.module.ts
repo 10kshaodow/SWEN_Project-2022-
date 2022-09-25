3dd53f285ee88fea032e3e5d0601aa291c3934f7
Ryan Current <rjc7379@g.rit.edu>
2022-09-25 13:36:40 -0400

    New: added search component UI

--- estore-ui/src/app/app.module.ts
+++ estore-ui/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AllInventoryComponent } from './components/all-inventory/all-inventory.component';
+import { SearchProductsComponent } from './components/search-products/search-products.component';
 
 @NgModule({
-  declarations: [AppComponent, AllInventoryComponent],
+  declarations: [AppComponent, AllInventoryComponent, SearchProductsComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
--- estore-ui/src/app/services/product/product.service.ts
+++ estore-ui/src/app/services/product/product.service.ts
@@ -15,11 +15,11 @@ export class ProductService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  /*
-    Ask the server for all of the products
-
-    returns an observable of product object array
-  */
+  /**
+   * GET: Ask the server for all of the products
+   * 
+   * returns an observable of product object array 
+   */
   getAllProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.apiUrl).pipe(
       tap((array) => {
@@ -29,21 +29,35 @@ export class ProductService {
     );
   }
 
-  /** PUT: update the product on the server */
-updateProduct(product: Product): Observable<any> {
-  return this.http.put(this.apiUrl, product, this.httpOptions).pipe(
-    tap(_ => console.log(`updated product id=${product.id}`)),
-    catchError(this.handleError<any>('updateProduct'))
-  );
-}
+  /* GET heroes whose name contains search term */
+  searchProducts(term: string): Observable<Product[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Product[]>(`${this.apiUrl}/?searchTerm=${term}`).pipe(
+      tap(x => x.length ?
+         console.log(`found products matching "${term}"`) :
+         console.log(`no products matching "${term}"`)),
+      catchError(this.handleError<Product[]>('searchHeroes', []))
+    );
+  }
 
-/** POST: add a new product to the server */
-addProduct(product: Product): Observable<Product> {
-  return this.http.post<Product>(this.apiUrl, product, this.httpOptions).pipe(
-    tap((newProduct: Product) => console.log(`added product w/ id=${newProduct.id}`)),
-    catchError(this.handleError<Product>('addProduct'))
-  );
-}
+  /** PUT: update the product on the server */
+  updateProduct(product: Product): Observable<any> {
+    return this.http.put(this.apiUrl, product, this.httpOptions).pipe(
+      tap(_ => console.log(`updated product id=${product.id}`)),
+      catchError(this.handleError<any>('updateProduct'))
+    );
+  }
+
+  /** POST: add a new product to the server */
+  addProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.apiUrl, product, this.httpOptions).pipe(
+      tap((newProduct: Product) => console.log(`added product w/ id=${newProduct.id}`)),
+      catchError(this.handleError<Product>('addProduct'))
+    );
+  }
 
   /*
     Simple Error handler will log it to browser console
